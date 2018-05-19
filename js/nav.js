117d062f341abc34168ada199f6db56678778a85
wcpg <577538931@qq.com>
2018-05-19 20:30:07 +0800

    qqq

---
+++ js/nav.js
@@ -0,0 +1,21 @@
+﻿$(function() {
+	$("#nav>ul>li").hover(function() {
+		$(this).addClass("sfhover");
+		$("#nav ul ul").slideUp("fast");
+		$(this).find("a:first").animate({"top": "12px"},"fast");
+		$(this).find("span").animate({"top": "-40px"},"fast");
+		if (!$(this).find("ul").is(":animated")) $(this).find("ul").slideDown("fast")
+	}
+	function() {
+		$(this).removeClass("sfhover");
+		$(this).find("a:first").animate({"top": "0px"},"fast");
+		$(this).find("span").animate({"top": "0px"},"fast");
+		if (!$(this).find("ul").is(":animated")) $(this).find("ul").slideUp("fast");
+		$("#nav ul ul").slideUp("fast")
+	})
+//Q
+SexyLightbox.initialize({color:'white',dir: '/z/theme/images/'});
+});
+
+
+
